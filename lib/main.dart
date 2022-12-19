42d43f4b2984c98cfde77d1a02650862711d5647
uqpyx <80069620+hichemBA67@users.noreply.github.com>
2022-12-19 20:43:19 +0100

    Section 2: 36

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,28 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      questionIndex++;
+      print(questionIndex);
+    });
+  }
+
+  var questions = [
+    'What\'s your favorite color?',
+    'What\'s your favorite animal?'
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -14,7 +35,18 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('My First App'),
         ),
-        body: Text('This is my default text!'),
+        body: Column(children: [
+          Text(questions[questionIndex]),
+          ElevatedButton(child: Text('Answer 1'), onPressed: answerQuestion),
+          ElevatedButton(
+              child: Text('Answer 2'),
+              onPressed: () => print('Answer 2 chosen!')),
+          ElevatedButton(
+              child: Text('Answer 3'),
+              onPressed: () => {
+                    print('Answer 3 chosen!'),
+                  }),
+        ]),
       ),
     );
   }
